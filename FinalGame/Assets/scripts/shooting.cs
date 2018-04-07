d873390e39b3424a03d1e8f1bfe4affa69ee8517
ascarangello <scarangello.a@husky.neu.edu>
2018-04-07 18:17:16 -0400

    added shooting animation transitions and made floor longer

--- FinalGame/Assets/scripts/shooting.cs
+++ FinalGame/Assets/scripts/shooting.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class shooting : MonoBehaviour {
-
+    Animator animate;
     public GameObject bullet;
     public Vector2 vel;
     bool canShoot = true;
@@ -12,25 +12,29 @@ public class shooting : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        animate = gameObject.GetComponent<Animator>();
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if(Input.GetKeyDown(KeyCode.Z) && canShoot)
         {
+            animate.SetTrigger("isShooting");
             Vector2 bullVector = new Vector2(transform.position.x + offset.x * transform.localScale.x, transform.position.y + offset.y);
                 GameObject go = Instantiate(bullet, bullVector,
                      Quaternion.identity);
 
                 go.GetComponent<Rigidbody2D>().velocity = new Vector2(vel.x * transform.localScale.x, vel.y);
                 StartCoroutine(CanShoot());
+            
 
-           
         }
-
         
-	}
+
+
+
+    }
 
 
 
